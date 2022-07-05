619307df235344d8823078b0036b013dc995bd48
AbreuNatali <abreu.natali@gmail.com>
2022-07-05 05:05:18 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n = -n;
+    const str = n.toString();    
+    let j;
+    let strRevStart = '';
+    let strRevEnd = '';
+    let middle = Math.floor((str.length)/2);
+        for (let i = 0; i < middle; i++) {
+        j = str.length - 1 - i;
+        strRevStart = strRevStart + str[j];
+        strRevEnd = str[i] + strRevEnd;                
+    }
+     
+    let result = ((str.length % 2) === 0)? Number(strRevStart + strRevEnd) : 
+    Number(strRevStart + str[middle] + strRevEnd);
+    return (result);
 }
+
